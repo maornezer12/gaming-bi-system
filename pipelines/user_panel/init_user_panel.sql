56da721a5993f05a220510296c22bea2f08f2c0f
maornezer <168960898+maornezer@users.noreply.github.com>
2025-10-26 14:30:06 +0200

    Fix: use dt instead of install_dt in WHERE clause for user_panel init

--- pipelines/user_panel/init_user_panel.sql
+++ pipelines/user_panel/init_user_panel.sql
@@ -5,20 +5,6 @@
  */
 
 CREATE OR REPLACE TABLE`{project}.{dataset_dst}.{table_dst}`
-(
-  user_id           STRING  OPTIONS(description='User unique ID'),
-  install_dt            DATE    OPTIONS(description='First activity date'),
---   Current_Version       STRING,
---   Current_Platform      STRING,
-  Level                 INTEGER,
-  t_active_days         INTEGER,
-  t_Session_Start       INTEGER,
-  t_Match_Start         INTEGER,
-  t_revenue             FLOAT64,
-  t_coins_gained        INTEGER,
---   t_XpEarned            INTEGER,
-  last_activity_dt      DATE    OPTIONS(description='Last activity date')
-)
 PARTITION BY {partition_att}
 OPTIONS (description = "{description}")
 AS
@@ -28,7 +14,7 @@ SELECT
 --   substring(max(concat(cast(dt as string), Version)), 11) AS version,
 --   substring(max(concat(cast(dt as string), Platform)), 11) AS platform
   MAX(level) AS level,
-  COUNT(1) AS t_active_days,
+  COUNT(DISTINCT dt) AS t_active_days,
   SUM(t_Session_Start) AS t_Session_Start,
   SUM(t_Match_Start) AS t_Match_Start,
   SUM(t_revenue) AS t_revenue,
@@ -36,5 +22,5 @@ SELECT
 --   SUM(t_XpEarned) AS t_XpEarned,
   MAX(dt) as last_activity_dt
 FROM `{project}.{dataset_src}.{table_src}`
-WHERE {partition_att} <= DATE("{date}")
-GROUP BY ALL;
+WHERE dt <= DATE("{date}")
+GROUP BY user_id;
